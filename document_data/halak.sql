8c73b1eeceecf9d43729c603ae5f0204922c744f
foldiB <xonia418@gmail.com>
2023-03-01 13:05:16 +0100

    documentdatabase

---
+++ document_data/halak.sql
@@ -0,0 +1,115 @@
+INSERT into species (fishid,usename, note, frequency, occurrence, protected) VALUES
+(35, 'süllö', 1830, 'A', 1, 0),
+(26, 'márna', 1887, 'C', 0, 0),
+(28, 'német bucó', 1931, 'C', 0, 1),
+(8, 'dévér keszeg', 1858, 'A', 1, 0),
+(14, 'garda', 1830, 'A', 1, 0),
+(27, 'menyhal', 1887, 'C', 0, 0),
+(18, 'karika keszeg' 1830, 'A', 1, 0),
+(6,	'compó',	1858,	'A',	1,	0),
+(36,	'szélhajtó küsz',	1858,'A',	1	,0),
+(32,	'selymes durbincs',	1887,	'C',	0,	1),
+(37,	'szivárványos ökle',	1887,	'A',	1,	1),
+(38,	'tarka géb'	,1840,	'C',	0,	1),
+(7,	'csuka',	1858,	'A',	1	,0),
+(15,	'harcsa',	1858,	'A',	1,	0),
+(40,	'vágó durbincs',	1887,	'A',	1	,0),
+(2,	'angolna'	,1890,	'A',	1,	0),
+(1,	'állas küsz',	1882,	'C'	,0,	1),
+(11,	'felpillantó küllõ',	1895,	'C'	,1,	1),
+(21,	'kövi csík'	,1858,	'B'	,1	,1),
+ ( 12,	'fenékjáró küllõ'	,1858,	'A',	1	,1),
+(41,	'vörösszárnyú keszeg'	,1858,	'A'	,1,	0),
+(19,	'kecsege'	,1887,	'C',	0,	0),
+(16,	'jászkeszeg'	,1895,	'C'	,1,	0),
+(31,	'sebes pisztráng'	,1906,	'C',	0	,0),
+(23,	'lápi póc',	1847,	'C',	1	,1),
+(13,	'fürge cselle'	,1858,	'B',	1,	1),
+(9,	'ezüst kárász'	,1858,	'B'	,1	0),
+(39,	'vágó csík'	,1858,	'B',	1	,1),
+(30,	'réti csík'	,1858	,'B'	,1,	1),
+(22,	'kurta baing'	,1897'	B'	1	,1),
+(34,	'sügér'	,1847,	'C',	1,	0),
+(4,	'balin'	,1858,	'A'	,1,	0),
+(17,	'kárász',	1858,'	A'	,1,	0),
+(5,	'bodorka'	,1858,	'A',	1,	0),
+(20,	'kõsüllõ'	,1861,	'A'	,1,	0),
+(25,	'magyar bucó'	,1931	,'C'	,0,	1),
+(29,	'ponty',	1858,	'A',	1,	0),
+(24,	'lapos keszeg'	,1858,	'C',	1,	0),
+(33,	'sujtásos küsz'	,1861	,'C',	0,	1),
+(3, 'bagoly keszeg'	,1887,	'C',	1,	0),
+(10,	'fejes domolyó'	,1858,	'C',	1,	0);
+
+
+
+INSERT INTO name (fihid, `landscape name`) VALUES
+  2	ángolna
+(2	ángvilla
+2	síkos ángolna
+3	bagoly
+3	bagókeszeg
+3	karikakeszeg
+3	száp
+3	szápakeszeg
+3	szápókeszeg
+4	bálind
+4	bálint
+4	balling
+4	baing
+4	baksa
+4	bolyín
+4	boín
+4	buczó
+4	búczókeczeg
+4	csabak
+4	fenekeszeg
+4	jászkeszeg
+4	kapókeszeg
+4	nagypénteki hal
+4	nagy-szélhal
+4	nyilkeszeg
+4	õn
+4	õnhal
+4	õnkeszeg
+4	õny
+4	õnyhal
+4	põlind
+4	ragadozó küsz
+4	ragadozó õn
+4	szellõkeszeg
+4	torzsáskeszeg
+4	vadászkeszeg
+4	vezérhal
+4	vizenjárókeszeg
+5	béka-hal
+5	boti
+5	botos
+5	butikóhal
+5	ebhal
+5	kölönte
+5	kolty
+5	kophal
+5	kopsa
+5	kutyahal
+5	ördöghal
+6	nyálkás czompó
+6	czigányhal
+6	gyászkeszeg
+6	haldoktor
+6	sárhal
+6	tathal
+6	vargahal
+6	varjúhal
+6	zöldike
+7	köz-csuka
+7	bugyli (növendék csuka)
+7	ezüstcsuka
+8	dévér
+8	dévértkeszeg
+8	bárdkeszeg
+8	dorzsmáskeszeg
+8	fahegykeszeg
+8	keszege
+8	feketeszárnyúkeszeg
+8	lapátkeszeg
